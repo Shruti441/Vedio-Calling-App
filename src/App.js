c6a26578549deb1625de8b1d6e0dae2a6ea2efaa
Shruti441 <varshneyshruti2003@gmail.com>
2023-09-13 11:25:50 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes , Route} from "react-router-dom";
+import"./App.css";
+import LobbyScreen from "./screen/lobby";
+import RoomPage from "./screen/room";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomPage/>} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
